c3b9444992d12b1fdcdf96cccc4b0bae949d6036
strookers <michael_k@mail.dk>
2016-11-02 11:49:54 +0100

    init commit

---
+++ IdentitySample/App_Start/ApplicationIdentityContext.cs
@@ -0,0 +1,41 @@
+﻿namespace IdentitySample
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using AspNet.Identity.MongoDB;
+	using Models;
+	using MongoDB.Driver;
+
+	public class ApplicationIdentityContext : IDisposable
+	{
+		public static ApplicationIdentityContext Create()
+		{
+			// todo add settings where appropriate to switch server & database in your own application
+			var client = new MongoClient("mongodb://admin:admin123@95.85.45.220:27017/");
+			var database = client.GetDatabase("userdb");
+			var users = database.GetCollection<ApplicationUser>("users");
+			var roles = database.GetCollection<IdentityRole>("roles");
+			return new ApplicationIdentityContext(users, roles);
+		}
+
+		private ApplicationIdentityContext(IMongoCollection<ApplicationUser> users, IMongoCollection<IdentityRole> roles)
+		{
+			Users = users;
+			Roles = roles;
+		}
+
+		public IMongoCollection<IdentityRole> Roles { get; set; }
+
+		public IMongoCollection<ApplicationUser> Users { get; set; }
+
+		public Task<List<IdentityRole>> AllRolesAsync()
+		{
+			return Roles.Find(r => true).ToListAsync();
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
